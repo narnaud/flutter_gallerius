39fc36b94e7b5f91829fe06ed2bc9297b3c028f0
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2022-09-26 15:07:20 +0200

    Add keyboard navigation (left/right)

--- lib/logic/gallery.dart
+++ lib/logic/gallery.dart
@@ -1,26 +1,62 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final galleryProvider = ChangeNotifierProvider((ref) => new Gallery());
+final galleryProvider = ChangeNotifierProvider((ref) => Gallery());
+
+class GalleryItem {
+  const GalleryItem({
+    required this.path,
+  });
+  final String path;
+}
 
 class Gallery extends ChangeNotifier {
   String? currentPath;
-  List<String> pictureFileNames = [];
+  String? currentItemPath;
+  List<GalleryItem> items = [];
 
   void setCurrentPath(String path) {
     currentPath = path;
-    pictureFileNames = _allPicturesInPath(path);
+    items = _allPicturesInPath(path);
+    if (items.isNotEmpty) {
+      currentItemPath = items.first.path;
+    }
     notifyListeners();
   }
 
-  List<String> _allPicturesInPath(String path) {
+  void setCurrentItemPath(String path) {
+    currentItemPath = path;
+    notifyListeners();
+  }
+
+  void navigateFocus(int delta) {
+    if (currentItemPath == null) {
+      return;
+    }
+    int index = items.indexWhere((item) => item.path == currentItemPath);
+    index = max(index + delta, 0);
+    currentItemPath = items[index].path;
+    notifyListeners();
+  }
+
+  void focusPrevious() {
+    navigateFocus(-1);
+  }
+
+  void focusNext({int distance = 1}) {
+    navigateFocus(1);
+  }
+
+  List<GalleryItem> _allPicturesInPath(String path) {
     final pictureDir = Directory(path);
     return pictureDir
         .listSync(recursive: false)
-        .map((item) => item.path)
-        .where((item) => (item.endsWith(".jpg") || item.endsWith(".png")))
+        .where((item) =>
+            (item.path.endsWith(".jpg") || item.path.endsWith(".png")))
+        .map((item) => GalleryItem(path: item.path))
         .toList(growable: false);
   }
 }
--- lib/widgets/gallery_widget.dart
+++ lib/widgets/gallery_widget.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gallerius/logic/gallery.dart';
 import 'package:path/path.dart' as p;
 
-class GalleryWidget extends StatelessWidget {
+class GalleryWidget extends StatefulWidget {
   const GalleryWidget({
     Key? key,
     required this.gallery,
@@ -15,23 +16,66 @@ class GalleryWidget extends StatelessWidget {
   final Function onImageClicked;
 
   @override
+  State<GalleryWidget> createState() => _GalleryWidgetState();
+}
+
+class _GalleryWidgetState extends State<GalleryWidget> {
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  // Handles the key events from the Focus widget and updates the
+  // _message.
+  KeyEventResult _handleKeyEvent(FocusNode node, KeyEvent event) {
+    setState(() {
+      if (event is KeyDownEvent) {
+        if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+          widget.gallery.focusPrevious();
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+          widget.gallery.focusNext();
+        } else if (event.logicalKey == LogicalKeyboardKey.enter) {
+          widget.onImageClicked(widget.gallery.currentItemPath);
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+          // ??
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+          // ??
+        }
+      }
+    });
+    return KeyEventResult.ignored;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView.extent(
-      primary: false,
-      padding: const EdgeInsets.all(16),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      maxCrossAxisExtent: 200.0,
-      children: gallery.pictureFileNames
-          .map(
-            (path) => GestureDetector(
-              onTap: () => onImageClicked(path),
-              child: GalleryImage(
-                path: path,
+    return Focus(
+      autofocus: true,
+      focusNode: _focusNode,
+      onKeyEvent: _handleKeyEvent,
+      child: GridView.extent(
+        primary: false,
+        padding: const EdgeInsets.all(16),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        maxCrossAxisExtent: 200.0,
+        children: widget.gallery.items
+            .map(
+              (item) => GestureDetector(
+                onTap: () {
+                  widget.gallery.setCurrentItemPath(item.path);
+                  widget.onImageClicked(item.path);
+                },
+                child: GalleryImage(
+                  path: item.path,
+                  selected: item.path == widget.gallery.currentItemPath,
+                ),
               ),
-            ),
-          )
-          .toList(),
+            )
+            .toList(),
+      ),
     );
   }
 }
@@ -40,22 +84,27 @@ class GalleryImage extends StatelessWidget {
   const GalleryImage({
     Key? key,
     required this.path,
+    required this.selected,
   }) : super(key: key);
 
   final String path;
+  final bool selected;
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: path,
-      child: Center(
-        child: PhysicalModel(
-          color: Colors.black,
-          elevation: 10,
-          shadowColor: Colors.black,
-          child: Tooltip(
-            message: p.basename(path),
-            child: Image.file(File(path)),
+    return Container(
+      color: selected ? Theme.of(context).focusColor : Colors.transparent,
+      child: Hero(
+        tag: path,
+        child: Center(
+          child: PhysicalModel(
+            color: Colors.black,
+            elevation: 10,
+            shadowColor: Colors.black,
+            child: Tooltip(
+              message: p.basename(path),
+              child: Image.file(File(path)),
+            ),
           ),
         ),
       ),
